30693c0b233285185dba47294d1c518115d1e41b
Muhammed Said Elsalih <166697203+MuhammetSaid@users.noreply.github.com>
2025-12-24 16:48:16 +0300

    Delete 941-sort-array-by-parity directory

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        ans = []; a = []
-        for i in nums:
-            if i%2 == 0: ans.append(i)
-            else: a.append(i)
-        return ans+a

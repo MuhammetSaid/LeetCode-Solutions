772ef39582b1ee882bbe0f0a98bad22ea8f9fad8
Muhammed Said Elsalih <166697203+MuhammetSaid@users.noreply.github.com>
2025-12-24 11:09:50 +0300

    Time: 16 ms (23.53%) | Memory: 32 MB (81.77%) - LeetSync

---
+++ 561-array-partition/array-partition.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int i = nums.size() - 2;
+        int ans = 0;
+        while (i >= 0) {
+            ans += nums[i];
+            i -= 2;
+        }
+        return ans;
+    }
+};

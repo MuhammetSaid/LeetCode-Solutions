3f9ba9704c8b6a1648ae2d7ecb0f54184c7acac0
Muhammed Said Elsalih <166697203+MuhammetSaid@users.noreply.github.com>
2025-12-24 16:49:53 +0300

    Delete 882-peak-index-in-a-mountain-array directory

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        n = len(arr);i=1;max = arr[0]
-        while arr[i] > max:
-            if arr[i] > max: max = arr[i]
-            i+=1
-        return i-1
